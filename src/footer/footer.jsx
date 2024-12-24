c2595b42c88ab67f70e25a0a3fb0d07c84daa4b8
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2024-12-24 16:53:33 +0300

    init commit

---
+++ src/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import footer_css from'./footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={footer_css.footer}>
+3
+    </div>
+  );
+};
---
+++ src/header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import header_css from'./header.module.css';
+
+export default function Header() {
+  return (
+    <div className={header_css.header}>
+      1
+    </div>
+  );
+};
